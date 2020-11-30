e971f2fe6aca41ee64fc1c4273efb0e71e4f8bfe
Gianluca Gippetto <gianluca.gippetto@gmail.com>
2020-11-30 16:08:40 +0100

    [test] Add test for age_grouper

--- tests/test_queries.py
+++ tests/test_queries.py
@@ -1,7 +1,7 @@
 import pytest
 
 import iccas
-from iccas.queries import FIELDS
+from iccas.queries import AGE_GROUPS, FIELDS
 
 
 # fmt: off
@@ -39,3 +39,18 @@ def test_cols_raises_for_invalid_fields():
         iccas.cols("*", "cases, deaths")
     with pytest.raises(ValueError, match="empty"):
         iccas.cols("*", "")
+
+
+@pytest.mark.parametrize('cuts, expected_values', [
+    (10, AGE_GROUPS),
+    (20,  ['0-19', '0-19', '20-39', '20-39', '40-59', '40-59',
+           '60-79', '60-79', '>=80', '>=80', 'unknown']),
+    (30,  ['0-29', '0-29', '0-29', '30-59', '30-59', '30-59',
+           '60-89', '60-89', '60-89', '>=90', 'unknown']),
+    ([20, 60],
+     ['0-19', '0-19', '20-59', '20-59', '20-59', '20-59',
+      '>=60', '>=60', '>=60', '>=60', 'unknown']),
+])
+def test_age_grouper(cuts, expected_values):
+    expected_output = dict(zip(AGE_GROUPS, expected_values))
+    assert iccas.age_grouper(cuts) == expected_output
